190df448066ed2074342f3e79a088c73db55bf0a
Ryan Gautier <ryangautier2@gmail.com>
2020-05-14 18:38:58 -0500

    Updates

---
+++ precache-manifest.04862fcec270156b94b78d9f250ffc5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f680809025dec081425ff965628891",
+    "url": "/project-manager/index.html"
+  },
+  {
+    "revision": "70f348643b6b7c5fca7d",
+    "url": "/project-manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c920064d5138b20b4c14",
+    "url": "/project-manager/static/js/2.89f37a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-manager/static/js/2.89f37a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f348643b6b7c5fca7d",
+    "url": "/project-manager/static/js/main.89eae72d.chunk.js"
+  },
+  {
+    "revision": "9b94bf99b0467618ea58",
+    "url": "/project-manager/static/js/runtime-main.1aeb210b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);

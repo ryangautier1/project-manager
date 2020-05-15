1fea9655098647958fe2817e87f9c18b75d919c1
Ryan Gautier <ryangautier2@gmail.com>
2020-05-14 22:01:25 -0500

    add sticky footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import EmployeeCard from "./components/EmployeeCard";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div>
       <Navbar />
       <EmployeeCard />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// This is a sticky footer. Styling to make it so is located in /src/index.css
+
+function Footer() {
+  return (
+    <footer className="border-t border-gray-200">
+      Footer Here
+    </footer>
+  );
+}
+
+export default Footer;

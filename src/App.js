d3f021c78ee192609bb70de605ae703914c1d104
Ryan Gautier <ryangautier2@gmail.com>
2020-05-16 17:58:44 -0500

    create project table page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer"
+import ProjectInfoTable from './components/ProjectInfoTable';
 
 function App() {
   return (
     <div>
       <Navbar />
-      
+      <ProjectInfoTable />
       <Footer />
     </div>
   );
---
+++ src/components/ProjectInfoTable/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import pic from "../../ryanpicturesquare.png";
+import "./style.css";
+
+function ProjectInfoTable() {
+  return (
+    <div className="mt-8 px-5 md:px-12 lg:px-20 w-full">
+     
+      <h2 className="text-md md:text-xl">
+        Project Title
+      </h2>
+      <hr />
+    </div>
+  );
+}
+
+export default ProjectInfoTable;

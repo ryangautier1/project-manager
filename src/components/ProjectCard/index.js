66e0eaa6c2eddfc0a74e38582c208b5d9fd5af0f
Ryan Gautier <ryangautier2@gmail.com>
2020-05-16 16:02:46 -0500

    add an assigned employee to project card

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import pic from "../../ryanpicturesquare.png"
 import "./style.css"
 
 function ProjectCard() {
@@ -10,14 +11,20 @@ function ProjectCard() {
         {/* Container for Card Heading */}
         <div className="mb-2 flex flex-row items-center">
           <img className="inline-block rounded-full h-16 w-16" src="https://via.placeholder.com/150x150" alt="placeholder" />
-          <div className="inline-block text-lg ml-4">
+          <div className="inline-block text-xl ml-4">
             <div>Project Title</div>
-            <div className="text-orange-500">Role Here</div>
           </div>
         </div>
         <hr />
-        <div className="mt-2">555-555-5555</div>
-        <div className="mt-2">email@email.com</div>
+        <div className="mt-2 text-gray-600">Project Descripton: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div className="mt-2">Assigned Team Members: </div>
+        <div className="my-2 flex flex-row items-center">
+          <img className="inline-block rounded-full h-12 w-12" src={pic} alt="placeholder" />
+          <div className="inline-block text-lg ml-4">
+            <div>Name Here</div>
+            <div className="text-orange-500">Role Here</div>
+          </div>
+        </div>
         <div className="mt-2">Department Here</div>
       </div>
     </div>
